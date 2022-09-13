9e38ce30fe0dc9bec8e72de752aa987ade4ce379
Nguti-xiv <avionistmatt@gmail.com>
2022-09-12 19:55:31 -0700

    "3-its lower case"

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: Print script that chacks for lowercase.
+ * Return; 1 if lowercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a'; && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (1);
+	}
+}

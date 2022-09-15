5e8bd96bed53add5b4b4a2ef04bb68ec50b293f3
Nguti-xiv <avionistmatt@gmail.com>
2022-09-15 10:46:55 -0700

    "print 0 to 9, skipping 2 and 4"

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * print_most _numbers - print numbers from 0 to 9.
+ * print_most _numbers - a function that prints the numbers, from 0 to 9.
  * Do not print 2 and 4.
  * followed by a new line.
- * Use `_putchar` twice.
- * Return: Always 0.
+ * Return: Always 0
  */
 void print_most_numbers(void)
 {

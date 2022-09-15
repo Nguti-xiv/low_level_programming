e627004a9023722c890758d3a5c4bf76df07cbd0
Nguti-xiv <avionistmatt@gmail.com>
2022-09-15 09:20:04 -0700

    "print from 0 to 9 skipping 2 and 4"

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
  */
 void print_most_numbers(void)
 {
-	int c = '0';
+	int i = '0';
 
-	for (; c <= '9'; c++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (c != '2' && c != '4')
-			_putchar(c);
+		if (i != 2 || i != 4)
+			_putchar(i);
 	}
 	_putchar('\n')
 }

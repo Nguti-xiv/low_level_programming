7d228b0b1e3f299180a3135ff29203968a330856
Nguti-xiv <avionistmatt@gmail.com>
2022-09-19 10:06:40 -0700

    "copies the string pointed to by src"

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = -1;
 
-	for (i = 0; *(src + 1) != '\0'; i++)
-	{
-		dest[i] = *(src +i);
-	}
-	dest[i] = '\0';
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
 
 	return (dest);
 }

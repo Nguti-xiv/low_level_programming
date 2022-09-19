5d967d7beec724f8bf713c322aeb152949248621
Nguti-xiv <avionistmatt@gmail.com>
2022-09-19 08:39:27 -0700

    "print a string to stdout"

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by a new line.
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}

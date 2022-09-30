129d971722c5ee4959cfc032cef2cfcb2c13b724
Nguti-xiv <avionistmatt@gmail.com>
2022-09-30 00:35:52 -0700

    "multiply two numbers"

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prog that takes the first two ints args and prints the product.
+ * @argc: number of cli args.
+ * @argv: array name.
+ * Return: 1, if not enough args passed, else 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+		j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
